8a9adb2e92d73f8610afd5411598258173598db2
frogin21 <firstlater@gmail.com>
2017-03-31 10:23:13 +0700

    modified coba

--- coba.java
+++ coba.java
@@ -3,6 +3,7 @@ public class Main{
     public static void main(String[] args) {
        public void coba(){
        System.out.println("Saya HEbat")
+       System.out.println("Test Git")
        }
 
     }
